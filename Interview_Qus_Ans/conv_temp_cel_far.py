530060296630aba1e6a3e63d010a5ee853a5598a
bpanda5498 <bpanda5498@gmail.com>
2024-05-14 19:17:53 +0530

    Temperature Convert

---
+++ Interview_Qus_Ans/conv_temp_cel_far.py
@@ -0,0 +1,3 @@
+celsius_temperature = float(input("Enter temperature in Celsius:"))
+fahrenheit_temperature = (celsius_temperature * 9/5) + 32
+print("Temperature in Fahrenheit:", fahrenheit_temperature)
