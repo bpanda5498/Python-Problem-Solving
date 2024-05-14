a6f9a43f36f2b1dae97493c9f8071afee4dad707
bpanda5498 <bpanda5498@gmail.com>
2024-05-14 19:30:35 +0530

    Temperature Convert to Celsius

---
+++ Interview_Qus_Ans/conv_temp_far_cel.py
@@ -0,0 +1,3 @@
+fahrenheit_temperature = float(input("Enter temperature in Fahrenheit:"))
+celsius_temperature = (fahrenheit_temperature - 32) * 5/9
+print("Temperature in Celsius: {:.2f}".format(celsius_temperature))
